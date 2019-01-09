99c52f3fae459338c2ec2025a7a768fd793991d0
Devi <devi.akula@hexagon.com>
2019-01-09 16:49:46 +0530

    Game State : Verifying an answer

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 const Button = (props) => {
-   return(
-     <div className="col-2">
-       <button className="btn" disabled={props.selected.length === 0}>=</button>
-     </div>
-   );
+  let button;
+  switch(props.ansIsCorrect){
+  case true: 
+    button = <button className="btn btn-success">
+                 <i className="fa fa-check"></i>
+             </button>;
+  break;
+  case false:
+    button = <button className="btn btn-danger">
+                 <i className="fa fa-times"></i>
+             </button>;
+  break;
+  default : 
+    button = <button onClick={props.checkAnswer} className="btn" disabled={props.selected.length === 0}>=</button>;
+  }
+  return(
+    <div className="col-2">
+      {button}
+    </div>
+  );
 };
 
 
--- src/Game.js
+++ src/Game.js
@@ -5,34 +5,40 @@ import Answer from './Answer';
 import Numbers from './Numbers';
 
 class Game extends React.Component{
-  state = {
-     selectedNums : [],
-     randomNumOfStars : Math.floor((Math.random() * 9)) + 1,
-  };
-  selectNumber = (clickedNum) => {
-     if(this.state.selectedNums.indexOf(clickedNum) >= 0) 
-        return;
-     this.setState( (prevState) => ({selectedNums : prevState.selectedNums.concat(clickedNum)}) );
-  };
-  unselectNumber = (clickedNum) => {
-     this.setState( (prevState) => ({selectedNums : prevState.selectedNums.filter(number => number !== clickedNum)}) 
-     );
-  };
-   render(){
-      return(
-        <div className="container">
-          <h3>Play Nine</h3>
-          <hr />
-          <div className="row">
-             <Stars randomNumOfStars={this.state.randomNumOfStars}/>
-             <Button selected={this.state.selectedNums}/>
-             <Answer selected={this.state.selectedNums} unselectNumber={this.unselectNumber}/>
-          </div>
-          <br />
-          <Numbers selected={this.state.selectedNums} selectNumber={this.selectNumber}/>
-        </div>
+   state = {
+      selectedNums : [],
+      randomNumOfStars : Math.floor((Math.random() * 9)) + 1,
+      ansIsCorrect : null,
+   };
+   selectNumber = (clickedNum) => {
+      if(this.state.selectedNums.indexOf(clickedNum) >= 0) 
+         return;
+      this.setState( (prevState) => ({selectedNums : prevState.selectedNums.concat(clickedNum)}) );
+   };
+   unselectNumber = (clickedNum) => {
+      this.setState( (prevState) => ({selectedNums : prevState.selectedNums.filter(number => number !== clickedNum)}) 
       );
-   }
+   };
+   checkAnswer = () => {
+      this.setState((prevState) => 
+      ({ ansIsCorrect : prevState.randomNumOfStars === prevState.selectedNums.reduce((acc, n) => acc+n, 0)})
+      );
+   };
+    render(){
+       return(
+         <div className="container">
+           <h3>Play Nine</h3>
+           <hr />
+           <div className="row">
+              <Stars randomNumOfStars={this.state.randomNumOfStars}/>
+              <Button selected={this.state.selectedNums} ansIsCorrect={this.state.ansIsCorrect} checkAnswer={this.checkAnswer}/>
+              <Answer selected={this.state.selectedNums} unselectNumber={this.unselectNumber}/>
+           </div>
+           <br />
+           <Numbers selected={this.state.selectedNums} selectNumber={this.selectNumber}/>
+         </div>
+       );
+    }
 }
 
 export default Game;
